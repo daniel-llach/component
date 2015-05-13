4a26c59aa025d6e4e299485c68ebe0db348e4f8b
daniel-llach <daniel.llach@gmail.com>
2015-05-13 09:51:16 -0400

    last changes

--- js/component/component.js
+++ js/component/component.js
@@ -9,7 +9,7 @@ define([
 
     var ExampleLayout = Marionette.LayoutView.extend({
         el: "body",
-        template: _.template("<div id=\"example\" style=\"width:100%;\"></div>"),
+        template: _.template("<div id=\"example\" style=\"width:100%;height:500px\"></div>"),
         className: "component"
     });
 
@@ -23,12 +23,23 @@ define([
             id: "component1",
             region: "main",
             channel: "c1",
-            title: "El título",
+            title: "El titulo",
             controls: [
                 {
+                    align: "left",
+                    name: "removeFilters"
+                },
+                {
                     align: "right",
-                    name: "sorting",
-                    icon: "filter",
+                    name: "btnModified"
+                },
+                {
+                    align: "right",
+                    name: "btnNew"
+                },
+                {
+                    align: "right",
+                    name: "btnDeleted"
                 }
             ],
             content: [
@@ -51,21 +62,103 @@ define([
             model: Component.ComponentModel
         });
 
-        Component.ComponentItemView = Marionette.ItemView.extend({
+        Component.ComponentItemView = Marionette.LayoutView.extend({
             tagName: "div",
             className: "component",
-            template: _.template('<div class="header"><h1><%- title %></h1></div><div class="controls"></div><div class="content"></div>'),
+            template: _.template('<div class="header"><h1><%- title %></h1></div><div class="controls"><div class="left"></div><div class="right"></div></div><div class="content"></div>'),
             regions: {
                 title: ".title",
                 controls: ".controls",
                 content: ".content"
             },
+
             onRender: function(){
                 console.log('Component.ComponentLayoutView: onRender');
+                var componentChannel = this.model.get('channel');
+                this.createRadioChannel(componentChannel);
 
+            },
+
+            onShow: function(){
                 var title = this.model.get('title');
-                console.log("title: ", title);
+                var controls = this.model.get('controls');
+
+                this.createControls(controls);
+                this.setDimentions(title, controls);
+            },
+
+            createRadioChannel: function(componentChannel){
+                // channel
+                var ComponentChannel = Radio.channel(componentChannel);
+            },
+
+            createControls: function(controls){
+                _.each(controls, function(control,i){
+                    var align = control.align;
+                    var name = control.name;
+                    var icon = control.icon;
+
+                    var ControlView = Backbone.Marionette.ItemView.extend({
+                        tagName: "li",
+                        id: name,
+                        className: "btn",
+                        template: _.template(''),
+                    });
+
+                    var ControlRegion = Backbone.Marionette.Region.extend({
+                        el: "." + align,
+
+                        attachHtml: function(view){
+                            // Some effect to show the view:
+                            this.$el.append(view.el);
+                          },
+
+                        initialize: function(options){
+                        // your init code, here
+                        },
+                    });
+
+                    Component.controlRegion = new ControlRegion();
+                    Component.controlRegion.show(new ControlView(), { preventDestroy: true });
+
+                });
+            },
+
+            setDimentions: function(title, controls){
+                var header = this.$el.find(".header");
+                var controlsPointer = this.$el.find(".controls");
+                var content = this.$el.find(".content");
+
+                var titleHeight = header.outerHeight();
+                var controlsHeight = controlsPointer.outerHeight();
+                var contentHeight = content.height();
+
+                // get region parent dimentions
+                var regionHeight = this.$el.parent().height();
+
+                // set content height
+                var baseContentHeight = regionHeight - titleHeight - controlsHeight;
+
+                // put height to content
+                this.$el.find(".content").height(baseContentHeight + "px");
+
+
+                if(title == "" || title == null){
+                    // eliminate header
+                    header.hide();
+                    // add title height to content height
+                    this.$el.find(".content").height(baseContentHeight + titleHeight + "px");
+                }
+
+                if(controls.length == 0 || controls == null){
+                    // eliminate header
+                    controlsPointer.hide();
+                    // add title height to content height
+                    this.$el.find(".content").height(baseContentHeight + controlsHeight + "px");
+                }
             }
+
+
         });
 
         Component.addInitializer(function(){
